847e05d5b4599f89a2d2452dbf68f8815dac6655
Robert Schnitman <robertschnitman@gmail.com>
2018-06-30 02:22:19 -0500

    Add files via upload

---
+++ src/reducechop.jl
@@ -0,0 +1,13 @@
+"""
+    reducechop(o, x, m)
+	
+Reduce a given collection x with a given binary operator o column-wise (m = 1) or row-wise (m = 2).
+
+OUTPUT: array/dataframe.
+
+# sources
+Main: https://github.com/robertschnitman/afpj
+
+"""
+
+reducechop(o, x, m) = mapslices(z -> reduce(o, z), x, m)
