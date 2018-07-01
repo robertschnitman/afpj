a8333dd9ac29be186cd0147b2690e0d23382532f
Robert Schnitman <robertschnitman@gmail.com>
2018-07-01 01:39:32 -0500

    Describe inputs; explicit reference to Wickham
    1. Move input description to the top so that readers may understand the shorthand inputs better.
    
    2. Explicitly reference the Wickham paper in case that

--- src/mapcat.jl
+++ src/mapcat.jl
@@ -1,18 +1,16 @@
 """
-mapcat(f, a, d = 1)
+mapcat(f, a, d = 1) # function, array of arrays, dimension.
 	
 Map a function `f` to an array of arrays `a`, concatenating each array vertically (d = 1, the default) or horizontally (d = 2).
 
-INPUTS: function, array of arrays, dimension.
-
 OUTPUT: array.
 
-Particularly useful when the array of arrays are multiple subsets of an original master array, further wanting to combine the arrays after mapping the subset-contingent function on each of them. Part of the "split-apply-combine" strategy.
+Particularly useful when the array of arrays are subsets of an original master array and that the desired output is the combined arrays after mapping the subset-contingent function on each of them. Part of the "split-apply-combine" strategy (Wickham 2011).
 
 # sources
 Main - https://github.com/robertschnitman/afpj/edit/master/src/mapcat.jl
 R equivalent - https://github.com/robertschnitman/afp/blob/master/R/do.bind.R
-Split-apply-combine strategy - https://www.jstatsoft.org/article/view/v040i01/v40i01.pdf
+Wickham, Hadley (2011). "The Split-Apply-Combine Strategy for Data Analysis". Journal of Statistical Software, vol. 40, issue 1. https://www.jstatsoft.org/article/view/v040i01/v40i01.pdf
 """
 
 function mapcat(f, a, d = 1)
