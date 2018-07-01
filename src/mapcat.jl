49fb5a66dc4084677c0279fefddee3a330df1928
Robert Schnitman <robertschnitman@gmail.com>
2018-07-01 00:55:51 -0500

    Default concatenation should be row-wise
    WHY: Particularly useful when the array of arrays are multiple subsets of an original master array, further wanting to combine the arrays after mapping the subset-contingent function on each of them. Part of the "split-apply-combine" strategy.
    
    https://www.jstatsoft.org/article/view/v040i01/v40i01.pdf

--- src/mapcat.jl
+++ src/mapcat.jl
@@ -1,18 +1,21 @@
 """
-mapcat(f, a, d)
+mapcat(f, a, d = 1)
 	
-Map a function `f` to an array of arrays `a`, concatenating each array vertically (d = 1) or horizontally (d = 2).
+Map a function `f` to an array of arrays `a`, concatenating each array vertically (d = 1, the default) or horizontally (d = 2).
 
-INPUTS: function, array of arrays, dimensiosn.
+INPUTS: function, array of arrays, dimension.
 
 OUTPUT: array.
+
+Particularly useful when the array of arrays are multiple subsets of an original master array, further wanting to combine the arrays after mapping the subset-contingent function on each of them. Part of the "split-apply-combine" strategy.
 
 # sources
 Main - https://github.com/robertschnitman/afpj/edit/master/src/mapcat.jl
 R equivalent - https://github.com/robertschnitman/afp/blob/master/R/do.bind.R
+Split-apply-combine strategy - https://www.jstatsoft.org/article/view/v040i01/v40i01.pdf
 """
 
-function mapcat(f, a, d)
+function mapcat(f, a, d = 1)
 
 apply = map(f, a)
 
@@ -22,7 +25,7 @@ apply = map(f, a)
 
   else
     
-	error("The dimensions input must either specify 1 (vertical concatenation) or 2 (horizontal).")
+    error("The dimensions input must either specify 1 (vertical concatenation) or 2 (horizontal).")
 	
   end
   
