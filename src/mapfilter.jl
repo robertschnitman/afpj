d64c2bcc5448c3efedf8a8331eb4cd4fec345c04
Robert Schnitman <robertschnitman@gmail.com>
2018-07-01 19:29:55 -0500

    Add files via upload

---
+++ src/mapfilter.jl
@@ -0,0 +1,17 @@
+"""
+mapfilter(f, p, x) # function map, function logical, collection)
+	
+Map a function `f` and then filter the result by a function `p` for a collection `x`.
+
+OUTPUT: array.
+
+# Example
+A = [1:10;]
+mapfilter(x -> x.^2, isodd, A) # == filter(isodd, map(x -> x.^2, A))
+
+# Source
+Main - https://github.com/robertschnitman/afpj/blob/master/src/mapfilter.jl
+R equivalent - https://github.com/robertschnitman/afp/blob/master/R/mapfilter.R
+"""
+
+mapfilter(f, p, x) = filter(p, map(f, x))
