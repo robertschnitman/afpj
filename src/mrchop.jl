59b3b5d5ee83535556cb950e2780a307dd0559da
Robert Schnitman <robertschnitman@gmail.com>
2018-07-13 17:10:40 -0500

    Match input description to its order
    Describing the inputs in the order they are presented is simpler than the same for the process.

--- src/mrchop.jl
+++ src/mrchop.jl
@@ -1,7 +1,7 @@
 """
 mrchop(f, o, x, d)
 	
-Map a function `f` to some data `x` and then reduce it column-wise (d = 1) or row-wise (d = 2). Calls mapslices() and mapreduce().
+Map a function `f` to and then reduce by `o` some data `x` column-wise (d = 1) or row-wise (d = 2). Calls mapslices() and mapreduce().
 
 INPUTS: function, (binary) operator, array/dataframe, dimension.
 
