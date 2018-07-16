0e44b03e991e63f4d5103bda6e8c9fcd64bc2560
Robert Schnitman <robertschnitman@gmail.com>
2018-07-15 20:52:47 -0500

    Add examples (for Issue #4)
    https://github.com/robertschnitman/afpj/issues/4

--- src/mapcat.jl
+++ src/mapcat.jl
@@ -7,7 +7,15 @@ OUTPUT: array.
 
 Particularly useful when the array of arrays are subsets of an original master array and that the desired output is the combined arrays after mapping the subset-contingent function on each of them. Part of the "split-apply-combine" strategy (Wickham 2011).
 
-# sources
+# Examples
+A = [[1:5;] [6:10;] [11:15;]] # Create 3 arrays (A, B, C).
+B = [[16:20;] [21:25;] [26:30;]]
+C = [[31:35;] [36:40;] [41:45;]]
+ABC = [A, B, C] # 3-element Array of Arrays
+mapcat(x -> x.^2, ABC)    # Row-wise concatenation (default d = 1) after squaring the 3 columns from each Array.
+mapcat(x -> x.^2, ABC, 2) # Column-wise concatenation of the same.
+
+# Sources
 Main - https://github.com/robertschnitman/afpj/blob/master/src/mapcat.jl
 R equivalent - https://github.com/robertschnitman/afp/blob/master/R/do.bind.R
 Wickham, Hadley (2011). "The Split-Apply-Combine Strategy for Data Analysis". Journal of Statistical Software, vol. 40, issue 1. https://www.jstatsoft.org/article/view/v040i01/v40i01.pdf
