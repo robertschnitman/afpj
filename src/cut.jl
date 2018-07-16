dc00dbd189f9535b6db40727693b89701b3bc104
Robert Schnitman <robertschnitman@gmail.com>
2018-07-16 17:21:44 -0500

    Add files via upload

---
+++ src/cut.jl
@@ -0,0 +1,17 @@
+"""
+cut(x, g) # array, group variable (position number).
+
+Partition a collection into an array of arrays with each element being a subset based on the given unique values. Similar to `split()` from R.
+
+OUTPUT: array/dataframe.
+
+# Examples
+code = vcat(fill(1, 33), fill(2, 33), fill(3, 33)) # 1st column is a category variable.
+A    = [code [101:199;] [201:299;]]
+cut(A, 1) # 3-element Array{Array{Int32,2},1}
+
+# Source
+Main - https://github.com/robertschnitman/afpj/blob/master/src/cut.jl
+"""
+
+cut(x, g) = map(i -> x[x[:, g] .== i, :], unique(x[:, g]))
---
+++ src/mapgrp.jl
@@ -0,0 +1,40 @@
+"""
+mapgrp(f, g, x) # function, group variable (column position), collection.
+
+Apply a function `f` based on a grouping variable `g` to a collection `x` . Calls map() and cut().
+
+OUTPUT: array/dataframe.
+
+# Examples
+code = vcat(fill(1, 33), fill(2, 33), fill(3, 33)) # 1st column is a category variable.
+A    = [code [101:199;] [201:299;]]
+mapgrp(maximum, 1, A)   # 3x3 Array. Maximum values by group.
+mapgrp(x -> x.^2, 1, A) # 99x3 Array. Squared values per group.
+
+# Source
+Main - https://github.com/robertschnitman/afpj/blob/master/src/mapgrp.jl
+cut() - https://github.com/robertschnitman/afpj/blob/master/src/cut.jl
+"""
+
+function mapgrp(f, g, x)
+  
+  # 1. Split
+  part1 = cut(x, g)
+  part2 = map(y -> y[:, 1:end .!= g], part1)
+  
+  # 2, Apply
+  # apply = vcat(map(y -> mapslices(z -> f(z), y, 1), part2)...)
+  apply = map(y -> mapslices(z -> f(z), y, 1), part2)
+  
+  # 3. Combine
+  nrow  = map(k -> size(k, 1), apply)[:, 1]
+       
+  col_code = vcat(map((y, n) -> fill(y, n), unique(x[:, g]), nrow)...)
+  
+  apply_cat = vcat(apply...)
+  
+  output = hcat(col_code, apply_cat)
+    
+  output
+  
+end
