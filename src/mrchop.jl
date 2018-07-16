7843af1e009aecf4ca2a252d7bcc09e22cf706f5
Robert Schnitman <robertschnitman@gmail.com>
2018-07-15 20:40:59 -0500

    Add examples (for Issue #2)

--- src/mrchop.jl
+++ src/mrchop.jl
@@ -1,13 +1,16 @@
 """
-mrchop(f, o, x, d)
+mrchop(f, o, x, d) #  function, (binary) operator, array/dataframe, dimension.
 	
 Map a function `f` to and then reduce by `o` some data `x` column-wise (d = 1) or row-wise (d = 2). Calls mapslices() and mapreduce().
 
-INPUTS: function, (binary) operator, array/dataframe, dimension.
+OUTPUT: array/data frame.
 
-OUTPUT: array/dataframe.
+# Examples
+A = [[1:5;] [6:10;] [11:15;]]
+mrchop(x -> x.^2, /, A, 1) # Column-wise reduction of squared elements.
+mrchop(x -> x.^2, /, A, 2) # Row-wise of the same.
 
-# sources
+# Sources
 Main - https://github.com/robertschnitman/afpj/blob/master/src/mrchop.jl
 R equivalent - https://github.com/robertschnitman/afp/blob/master/R/mrchop.R
 """
