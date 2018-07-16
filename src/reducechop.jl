e4714ce7e508b0cc18e9b8b065de5dd02e605880
Robert Schnitman <robertschnitman@gmail.com>
2018-07-15 21:01:00 -0500

    Add examples (for Issue #5)
    https://github.com/robertschnitman/afpj/issues/5

--- src/reducechop.jl
+++ src/reducechop.jl
@@ -5,7 +5,12 @@ Reduce a given collection x with a given binary operator o column-wise (d = 1) o
 
 OUTPUT: array/dataframe.
 
-# source
+# Examples
+A = [[1:5;] [6:10;] [11:15;]]
+reducechop(*, A, 1) # == mapslices(z -> reduce(*, z), A, 1)
+reducechop(*, A, 2) # == mapslices(z -> reduce(*, z), A, 2)
+
+# Source
 Main - https://github.com/robertschnitman/afpj/blob/master/src/reducechop.jl
 """
 
