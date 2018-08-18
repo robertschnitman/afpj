a9a5ca5e31c18fb8783472cb065a182a234919ac
Robert Schnitman <robertschnitman@gmail.com>
2018-08-18 03:32:00 -0500

    Rename to avoid confusion with R's cut()
    This function is similar to R's split(), not cut(). Avoid confusion by renaming to rip().

--- src/rip.jl
+++ src/rip.jl
@@ -1,5 +1,5 @@
 """
-cut(x, g) # array, group variable (position number).
+rip(x, g) # array, group variable (position number).
 
 Partition a collection into an array of arrays with each element being a subset based on the given unique values. Similar to `split()` from R.
 
@@ -8,10 +8,10 @@ OUTPUT: array/dataframe.
 # Examples
 code = vcat(fill(1, 33), fill(2, 33), fill(3, 33)) # 1st column is a category variable.
 A    = [code [101:199;] [201:299;]]
-cut(A, 1) # 3-element Array{Array{Int32,2},1}
+rip(A, 1) # 3-element Array{Array{Int32,2},1}
 
 # Source
-Main - https://github.com/robertschnitman/afpj/blob/master/src/cut.jl
+Main - https://github.com/robertschnitman/afpj/blob/master/src/rip.jl
 """
 
-cut(x, g) = map(i -> x[x[:, g] .== i, :], unique(x[:, g]))
+rip(x, g) = map(i -> x[x[:, g] .== i, :], unique(x[:, g]))
