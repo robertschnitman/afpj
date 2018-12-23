3428fba60edf6a49cf2044533cfc555529a77f5e
Robert Schnitman <robertschnitman@gmail.com>
2018-12-23 02:51:31 -0600

    Simplify
    Be concise to increase readability and efficiency.

--- src/mapdims.jl
+++ src/mapdims.jl
@@ -1,7 +1,7 @@
 """
-mapdims(f, x) # function, 2D array
+mapdims(f, x) # function, 2D Array
 
-Map over both dimensions of the 2D array and return as an array-of-arrays. First element contains the row-wise results; the second holds the column-wise computations.
+Map over both dimensions of the 2D Array and return as an Array of Arrays. First element contains the row-wise results; the second holds the column-wise computations.
 
 # Example
 A = [1:5 6:10;]
@@ -12,12 +12,4 @@ Main - https://github.com/robertschnitman/afpj/blob/master/src/mapdims.jl
 R equivalent - https://github.com/robertschnitman/afp/blob/master/R/mapdims.R
 """
 
-function mapdims(f, x)
-  row = mapslices(f, x, 2)
-  col = mapslices(f, x, 1)
-  
-  out = [[row]; [col]]
-  
-  out
-  
-end
+mapdims(f, x) = map(d -> mapslices(f, x, d), [2; 1])
