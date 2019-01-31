25fb40b5617f9d645f0d256cb5e3485b3d39c608
Robert Schnitman <robertschnitman@gmail.com>
2019-01-30 23:29:24 -0600

    Be concise
    WHY: Concision is one purpose of the library.

--- src/maps.jl
+++ src/maps.jl
@@ -7,4 +7,4 @@ Abbreviation of `mapslices()`. Maintains same functionality.
 Main - https://github.com/robertschnitman/afpj/blob/master/src/maps.jl
 """
 
-maps(f, A, dims) = mapslices(f, A, dims)
+maps = mapslices
