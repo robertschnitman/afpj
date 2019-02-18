be136ab2123e78fdf31db6215105ced487049294
Robert Schnitman <robertschnitman@gmail.com>
2019-02-18 14:41:15 -0600

    Update mapc.jl

--- src/mapc.jl
+++ src/mapc.jl
@@ -1,14 +1,11 @@
 """
 mapc(f, x)
 	
-Map a function f to some data x column-wise. Analagous to sapply() in R.
+Map a function f to some data x column-wise. Analagous to mapslices(f, x, 1).
 
 INPUTS: function, array/dataframe.
 
 OUTPUT: array/dataframe.
-
-# Examples
-See https://github.com/robertschnitman/afp.
 
 """
 
