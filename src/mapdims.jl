adf1b0dd30456460dc65fd805b2e3fc57855abf8
Robert Schnitman <robertschnitman@gmail.com>
2019-02-23 02:37:19 -0600

    Consolidate
    Also, be consistent with Julia conventions.

--- src/mapdims.jl
+++ src/mapdims.jl
@@ -1,15 +1,21 @@
 """
 mapdims(f, x) # function, 2D Array
+mapc(f, x) # Analagous to mapslices(f, x, 1)
+mapr(f, x) # Analagous to mapslices(f, x, 2)
 
-Map over both dimensions of the 2D Array and return as an Array of Arrays. First element contains the row-wise results; the second holds the column-wise computations.
+The function `mapdims()` maps over both dimensions of the 2D Array and return as an Array of Arrays. First element contains the row-wise results; the second holds the column-wise computations. The functions `mapc()` and `mapr()` call `mapslices()` to execute operations column-wise or row-wise respectively.
 
-# Example
+# Examples
 A = [1:5 6:10;]
 mapdims(mean, A)
+mapc(mean, A)
+mapr(mean, A)
 
 # Source
 Main - https://github.com/robertschnitman/afpj/blob/master/src/mapdims.jl
 R equivalent - https://github.com/robertschnitman/afp/blob/master/R/mapdims.R
 """
 
-mapdims(f, x) = map(d -> mapslices(f, x, d), [2; 1]) # Row-wise results first to coincide with the R-equivalent function.
+mapdims(f, x) = map(d -> mapslices(f, x, d), [1; 2]) # Go in Julia order.
+mapc(f, x) = mapslices(f, x, 1)
+mapr(f, x) = mapslices(f, x, 2)
