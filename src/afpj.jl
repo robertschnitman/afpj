aaf5db209fbcd971d4ddc34a68b8db8f0b5b4189
Robert Schnitman <robertschnitman@gmail.com>
2019-02-23 01:11:25 -0600

    Add files via upload
    Sorry to keep you waiting.

---
+++ src/afpj.jl
@@ -0,0 +1,15 @@
+### Load afpj module
+__precompile__()
+
+module afpj
+	
+include("mapby.jl")
+include("mapc.jl")
+include("mapcat.jl")
+include("mapdims.jl")
+include("mapr.jl")
+include("mapc.jl")
+include("mrchop.jl")
+include("rip.jl")
+	
+end
