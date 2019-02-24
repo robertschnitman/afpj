0e693fc7daa75a9d6126c8377db5a034bb0d7c62
Robert Schnitman <robertschnitman@gmail.com>
2019-02-23 18:13:23 -0600

    Consolidate

--- src/mop.jl
+++ src/mop.jl
@@ -1,16 +1,20 @@
 """
-mop(f, o, x, d) # (summary statistic) function, (binary) operator, array, dimension
+mop(f, o, x, d)   # (summary statistic) function, (binary) operator, array, dimension
+sweep(s, o, x, d) # summary statistic, (binary) operator, array, dimension
 
-Operate on an array by a summary statistic function. Based on the `afp` R library function `mop()`, which has origins in the R function `sweep()`.
+Operate on an array by a a summary statistic function or explicitly summary statistics. Based on the `afp` R library function `mop()` and the base R function `sweep()` respectively. 
 
 # Example
 A = [1:10 11:20 21:30;]
-mop(median, /, A, 1) # == mapslices(x -> x./median(x), A, 1)
+mop(median, /, A, 1)            # == mapslices(x -> x./median(x), A, 1)
+sweep(median(A[:, 1]), /, A, 1) # == mapslices(x -> x./median(A[:, 1]), A, 1) == A./median(A[:, 1])
 
 # Sources
-Main - https://github.com/robertschnitman/afpj/blob/master/src/mop.jl
-R equivalent - https://github.com/robertschnitman/afp/blob/master/R/mop.R
-sweep() - https://stat.ethz.ch/R-manual/R-devel/library/base/html/sweep.html
+1. Main - mop() - https://github.com/robertschnitman/afpj/blob/master/src/mop.jl
+2. Main - sweep() - https://github.com/robertschnitman/afpj/blob/master/src/sweep.jl
+3. R equivalent - mop() - https://github.com/robertschnitman/afp/blob/master/R/mop.R
+4. R origin - sweep() - https://stat.ethz.ch/R-manual/R-devel/library/base/html/sweep.html
 """
 
 mop(f, o, x, d) = mapslices(y -> o(y, f(y)), x, d)
+sweep(s, o, x, d) = mapslices(y -> o(y, s), x, d)
